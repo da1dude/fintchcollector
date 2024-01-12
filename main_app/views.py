bf442c72c0c84f2701430336b4f224e1dc4493b6
Paul K <pkerschieter@gmail.com>
2024-01-12 15:31:22 -0600

    initial django setup

---
+++ main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+finches = [
+    {'name': 'Bob', 'description': 'blue and red', 'age': 3},
+    {'name': 'Frank', 'description': 'black and blue', 'age': 2},
+    {'name': 'Carl', 'description': 'gold and silver', 'age': 4},
+]
+
+
+# Create your views here.
